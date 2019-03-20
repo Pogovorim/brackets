f48cef8c3c19ab0e5ddb2ef759d32cd77df48d66
Stanislav Sokolov <47755623+Pogovorim@users.noreply.github.com>
2019-03-20 17:26:07 +0300

    Updating our file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if(str.length % 2 !== 0){
+    return false;
+    
+} else {
+
+    for(var i = str.length - 1; i >= 0; i--){
+        for (var a = 0; a < bracketsConfig.length; a++) {
+            if (str[i] === bracketsConfig[a][0]) {
+                str = str.replace(bracketsConfig[a].join(''), '');
+            }
+        }
+    }
+
+    return str === '';
+}
 }
